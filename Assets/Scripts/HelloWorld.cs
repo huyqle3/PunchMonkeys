f034ea7d4cc4ab49c6142ca1f042c74fe3d3d4c4
Huy Le <huyle333@bu.edu>
2017-10-26 16:50:40 -0700

    Hello world of Unity

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class HelloWorld : MonoBehaviour {
+    public GameObject myCamera;
+    public GameObject myHands;
+   // public HandMod
+	// Use this for initialization
+	void Start () {
+        Debug.Log("hello world from Liao on Start()");
+        // Wrong code: it means this->GetComponent <>, not searching from top level!
+        // myCamera = GetComponent<Camera>();
+        myCamera = GameObject.Find("Camera");
+
+        if (myCamera)
+        {
+            Debug.Log("Found the camera");
+            myHands=myCamera.GetComponent<HandModels>();
+    
+        }
+        else
+        {
+            Debug.Log("Not found the camera");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++ README.md
@@ -7,3 +7,18 @@ Insteading of sitting on a chair and typing, we want to test if we can
 get programmers standup and punch a virtual keyboard in their virtual view. 
 
 So programmers can get some excercises while working!
+
+
+Development environment
+
+Hardware
+* Leap Motion Controller
+ 
+Software
+* Bash on Ubuntu on Windows
+* Console2 (a better Windows Command Prompt)
+* Unity 5.6.1 64 bit
+* Leap Motion Controller (Desktop) software 
+
+References
+* https://api.leapmotion.com/documentation/csharp/examples/Unity_Examples.html
